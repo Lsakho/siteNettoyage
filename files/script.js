efc3de27e5798689a87b6a485f6139fc8923be00
Lsakho <sakho.lamine2k19@gmail.com>
2024-01-03 13:13:35 +0100

    refactoring pages

--- files/script.js
+++ files/script.js
@@ -9,35 +9,3 @@
         // Ajouter un marqueur à la carte
         L.marker([46.17887,6.12119]).addTo(mymap)
             .bindPopup('Seck Sevice Nettoyage');
-
-//image slider
-
-
-// Définir les variables
-const carouselContainer = document.querySelector(".carousel-container");
-const slides = carouselContainer.querySelectorAll(".carousel-item");
-
-// Initialiser le nombre de slide
-let slideIndex = 0;
-
-// Écouteur d'événements
-carouselContainer.addEventListener("keydown", function(event) {
-  // Définir la direction du défilement
-  const direction = event.keyCode === 38 ? -1 : 1;
-
-  // Déplacer le slide
-  slideIndex += direction;
- console.log("js8 la");
-  // Vérifier que le slide est dans les limites du carousel
-  if (slideIndex < 0) {
-    slideIndex = slides.length - 1;
-  } else if (slideIndex >= slides.length) {
-    slideIndex = 0;
-  }
-
-  // Définir le slide actif
-  slides[slideIndex].classList.add("active");
-
-  // Déplacer le slide
-  carouselContainer.scrollLeft += direction * slides[0].offsetWidth;
-});
